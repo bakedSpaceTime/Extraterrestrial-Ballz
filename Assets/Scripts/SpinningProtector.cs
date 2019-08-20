fef72cd338d479eab5a2964bfcfea2ce78355f3f
bakedSpaceTime <tushyais@gmail.com>
2019-08-20 02:22:27 -0700

    allow for protector to rotate along different axis
    WIP

--- Assets/Scripts/SpinningProtector.cs
+++ Assets/Scripts/SpinningProtector.cs
@@ -7,7 +7,12 @@ public class SpinningProtector : MonoBehaviour
     public GameObject PiecePrefab;
     public Transform parent;
 
+    public bool xAxis;
+    public bool yAxis;
+    public bool zAxis;
+
     private Vector3 origin;
+    private Vector3 rotationOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -17,24 +22,63 @@ public class SpinningProtector : MonoBehaviour
 
     void CreateProtector()
     {
+        AxisCheck();
         int pieces = 8;
 
         origin = parent.position;
+        //rotationOffset = parent.rotation;
 
         for(int i = 0; i < pieces; i++)
         {
-            Vector3 position = Vector3.zero;
+            if (xAxis)
+            {
+                Vector3 position = Vector3.zero;
+                Quaternion rotation = Quaternion.Euler(45 * i, 0, 0) * PiecePrefab.transform.rotation;
+
+                Instantiate(PiecePrefab, position + origin, rotation, parent);
+            }
+            else if(zAxis)
+            {
+                Vector3 position = Vector3.zero;
+                Quaternion rotation = Quaternion.Euler(0, 0, 45 * i) * PiecePrefab.transform.rotation;
+
+                Instantiate(PiecePrefab, position + origin, rotation, parent);
+            }
+            else
+            {
+                Vector3 position = Vector3.zero;
+                Quaternion rotation = Quaternion.Euler(0, 45 * i, 0) * PiecePrefab.transform.rotation;
+
+                Instantiate(PiecePrefab, position + origin, rotation, parent);
+            }
             
-            // Use to create eliptical orbits
-            //new Vector3(Mathf.Sin((45 * i) * Mathf.Deg2Rad), 0, Mathf.Cos((45 * i) * Mathf.Deg2Rad));
-
-            Quaternion rotation = Quaternion.Euler(0, 45 * i, 0);
-
-            //Instantiate(PiecePrefab, position, rotation, PiecePrefab.transform);
-            Instantiate(PiecePrefab, position + origin, rotation, parent);
            
         }
 
         PiecePrefab.SetActive(false);
     }
+
+    private void AxisCheck()
+    {
+        if(xAxis && yAxis && zAxis)
+        {
+            xAxis = false;
+            zAxis = false;
+        }
+        else if(yAxis && xAxis)
+        {
+            xAxis = false;
+        }
+        else if(xAxis && zAxis)
+        {
+            zAxis = false;
+        }
+        else if(zAxis && yAxis)
+        {
+            yAxis = false;
+        }
+    }
 }
+
+// Use to create eliptical orbits
+//new Vector3(Mathf.Sin((45 * i) * Mathf.Deg2Rad), 0, Mathf.Cos((45 * i) * Mathf.Deg2Rad));
