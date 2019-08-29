553c9aa7fc81ccec6f4c59f1e1f88e5929c81c42
bakedSpaceTime <tushyais@gmail.com>
2019-08-29 00:41:01 -0700

    test camera script
    allows user to control camera position

---
+++ Assets/Scripts/camera.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camera : MonoBehaviour
+{
+    public float sensitivity;
+    public CinemachineVirtualCamera cam;
+    public FloatingJoystick stik;
+
+    //private Component comp;
+
+    void Start()
+    {
+        //comp = cam.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var comp = cam.GetCinemachineComponent<CinemachineOrbitalTransposer>();
+        comp.m_XAxis.m_InputAxisValue = stik.Horizontal * sensitivity;
+
+        Debug.Log(comp.m_XAxis.m_InputAxisValue);
+    }
+}
